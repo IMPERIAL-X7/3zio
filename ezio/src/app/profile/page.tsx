9234d8a4460d4663d0b327183c982f4d70972494
Arnav-Panjla <arnavpanjla@gmail.com>
2025-10-25 11:01:27 +0530

    added front end

--- ezio/src/app/profile/page.tsx
+++ ezio/src/app/profile/page.tsx
@@ -1,30 +1,24 @@
 "use client";
 
 import ConnectWallet from "@/components/blocks/connect-wallet";
-import Nexus from "@/components/nexus";
 import NexusInitButton from "@/components/nexus-init";
 import ProfileCard from "@/components/profile/ProfileCard";
 import { useNexus } from "@/providers/NexusProvider";
 
-export default function Home() {
+export default function ProfilePage() {
   const { nexusSDK } = useNexus();
+
   return (
     <div className="font-sans flex flex-col items-center justify-items-center min-h-screen p-8 pb-20 gap-y-6 sm:p-20">
-      <h1 className="text-5xl font-semibold z-10">
-        EZIO
-      </h1>
+      <h1 className="text-3xl font-semibold z-10">Profile</h1>
       <h2 className="text-lg font-semibold z-10">
-        A privacy-preserving protocol for transferring funds.
+        Manage your balances and transactions
       </h2>
       <div className="flex gap-x-4 items-center justify-center z-10">
         <ConnectWallet />
-        {/* <NexusInitButton /> */}
+        <NexusInitButton />
       </div>
-      {/* Bypassing Nexus init for development */}
-      <div className="w-full flex flex-col items-center gap-y-6">
-        <ProfileCard />
-        {nexusSDK?.isInitialized() && <Nexus />}
-      </div>
+      {nexusSDK?.isInitialized() && <ProfileCard />}
       <div
         className="fixed inset-0 z-0"
         style={{
